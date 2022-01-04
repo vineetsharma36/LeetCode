36b2380be4f0065c9a0a083084154a689b1d69dc
vineetsharma36 <64974314+vineetsharma36@users.noreply.github.com>
2022-01-04 19:05:12 +0530

    Time: 7 ms (28.42%), Space: 6.6 MB (75.47%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         int x=0,y=0;
-        unordered_map<char,int> mp1,mp2;
-        for(auto& x:secret){
-            mp1[x]++;
-        }
-        for(auto& x:guess){
-            mp2[x]++;
-        }
+        vector<int> v1(10,0),v2(10,0);
         for(int i=0;i<secret.length();i++){
             if(secret[i]==guess[i]){
                 x++;
-                mp1[secret[i]]--;
-                mp2[secret[i]]--;
+            }
+            else{
+                v1[secret[i]-'0']++;
+                v2[guess[i]-'0']++;
             }
         }
-        for(auto x: mp1){
-            if(mp2.find(x.first)!=mp2.end())
-            {
-                y+=min(x.second, mp2[x.first]);
-            }
+        for(int i=0;i<10;i++){
+            y+=min(v1[i],v2[i]);
         }
         string res=to_string(x)+'A'+to_string(y)+'B';
         return res;
