6597ff9c208dcc1fb7bee84e2ecc51379d39b413
Vineet Kumar Sharma <64974314+vineetsharma36@users.noreply.github.com>
2022-01-13 15:44:34 +0530

    Time: 589 ms (29.52%), Space: 89.8 MB (63.52%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -5,8 +5,8 @@ public:
         int start=points[0][0],end=points[0][1];
         int res=1;
         for(int i=1;i<points.size();i++){
-            if(points[i][0]>=start && points[i][0]<=end){
-                start=max(start,points[i][0]);
+            if(points[i][0]<=end){
+                start=points[i][0];
                 end=min(end, points[i][1]);
             }
             else{
