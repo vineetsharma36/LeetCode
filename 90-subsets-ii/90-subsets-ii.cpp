ba1ed516e1ac712e6b5ba9ebab12e34fd8ed40bb
Vineet Kumar Sharma <64974314+vineetsharma36@users.noreply.github.com>
2022-01-18 21:11:37 +0530

    Time: 2 ms (76.72%), Space: 7.5 MB (73.00%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     void helper(vector<int>& nums, int i, int n, vector<int> &v, vector<vector<int>> &res){
-        if(i==n){
-            res.push_back(v);
-            return;
+        res.push_back(v);
+        for(int k=i;k<n;k++){
+            if(i!=k && nums[k]==nums[k-1])
+                continue;
+            v.push_back(nums[k]);
+            helper(nums, k+1, n, v, res);
+            v.pop_back();
         }
-        v.push_back(nums[i]);
-        helper(nums, i+1, n, v, res);
-        v.pop_back();
-        helper(nums, i+1, n, v, res);
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int i=0,n=nums.size();
-        vector<vector<int>> res, fin;
+        vector<vector<int>> res;
         vector<int> v;
         helper(nums, i, n, v, res);
-        sort(res.begin(),res.end());
-        set<vector<int>> s;
-        v.clear();
-        for(int i=0;i<res.size();i++){
-            v=res[i];
-            sort(v.begin(),v.end());
-            if(s.find(v)==s.end()){
-                fin.push_back(v);
-                s.insert(v);
-            }
-        }
-        return fin;
+        return res;
     }
 };
