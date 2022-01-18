b846bb2565581886c95ca6f239f49302eab5f74d
Vineet Kumar Sharma <64974314+vineetsharma36@users.noreply.github.com>
2022-01-19 01:18:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (44.42%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    bool safe(vector<string> &board, int row, int col, int n){
-        int i=row, j=col;
-        while(row>=0 && col>=0){
-            if(board[row--][col--]=='Q')
-                return false;
-        }
-        row=i;
-        col=j;
-        while(col>=0){
-            if(board[row][col--]=='Q')
-                return false;
-        }
-        row=i;
-        col=j;
-        while(row<n && col>=0){
-            if(board[row++][col--]=='Q')
-                return false;
-        }
-        return true;
-    }
-    
-    void helper(int col, int n, vector<string> &board, int &res){
+    void helper(int col, int n, vector<string> &board, int &res, vector<int> &side, vector<int> &lower, vector<int> &upper){
         if(col==n)
         {
             res++;
             return;
         }
         for(int row=0;row<n;row++){
-            if(safe(board, row, col, n)){
+            if(side[row]==0 && lower[row+col]==0 && upper[n-1+col-row]==0){
                 board[row][col]='Q';
-                helper(col+1, n, board, res);
+                side[row]=1;
+                lower[row+col]=1;
+                upper[n-1+col-row]=1;
+                helper(col+1, n, board, res, side, lower, upper);
                 board[row][col]='.';
+                side[row]=0;
+                lower[row+col]=0;
+                upper[n-1+col-row]=0;
             }
         }
     }
@@ -40,10 +25,11 @@ public:
         int res=0;
         vector<string> board(n);
         string s(n,'.');
+        vector<int> side(n,0), lower(2*n-1, 0), upper(2*n-1, 0);
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        helper(0, n, board, res);
+        helper(0, n, board, res, side, lower, upper);
         return res;
     }
 };
