335fdb14d6959c1fcb9ee2f50df343bffc164b2e
Vineet Kumar Sharma <64974314+vineetsharma36@users.noreply.github.com>
2022-04-07 09:32:00 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.96%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto& x: stones)
+            pq.push(x);
+        while(pq.size()>1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            if(x==y)
+                continue;
+            else
+            {
+                if(x>y)
+                    pq.push(x-y);
+                else
+                    pq.push(y-x);
+            }
+        }
+        if(pq.size()==0)
+            return 0;
+        return pq.top();
+    }
+};
